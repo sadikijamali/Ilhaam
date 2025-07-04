f601f28d8d989de3b64a50cfb5ca4c262f800374
PRINCE TECHNOLOGY <princerwanda2003@gmail.com>
2025-07-04 22:12:19 +0300

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,3 +1,4 @@
+
 async function unmuteCommand(sock, chatId) {
     await sock.groupSettingUpdate(chatId, 'not_announcement'); // Unmute the group
     await sock.sendMessage(chatId, { text: 'The group has been unmuted.' });
