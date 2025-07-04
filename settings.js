3ebcbe4b9d9646d458ccb14cf4cd57563067e820
PRINCE TECHNOLOGY <princerwanda2003@gmail.com>
2025-07-04 19:10:30 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'BWB XMD',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "BWB XMD",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2557634140', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
