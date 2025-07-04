cab6c9cb23cffc05f77d16ca372a9ce8f8a1267a
PRINCE TECHNOLOGY <princerwanda2003@gmail.com>
2025-07-04 19:17:23 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "BWB XMD",
   botOwner: 'Professor', // Your name
-  ownerNumber: '2557634140', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '255763834140', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
