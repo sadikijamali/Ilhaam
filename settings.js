608c8b0a57b68f9ef0d626293a27fbc9091faf59
THE KINGDOM <teahp22@gmail.com>
2025-07-05 17:12:04 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'BWB XMD',
+  packname: 'KINGDOM MD',
   author: '‎',
-  botName: "BWB XMD",
-  botOwner: 'Princetech', // Your name
-  ownerNumber: '255763834140', //Set your number here without + symbol, just add country code & number without any space
+  botName: "KINGDOM MD",
+  botOwner: 'kingdom', // Your name
+  ownerNumber: '255654667145', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.6",
 };
