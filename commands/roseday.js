0c64299e0e9988e1c4323cd0697a5e7d8c31cb8e
PRINCE TECHNOLOGY <princerwanda2003@gmail.com>
2025-07-05 08:38:42 +0300

    Update roseday.js

--- commands/roseday.js
+++ commands/roseday.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 async function rosedayCommand(sock, chatId, message) {
     try {
-        const shizokeys = 'knightbot';
+        const shizokeys = 'BWB XMD';
         const res = await fetch(`https://api.shizo.top/quote/roseday?apikey=${shizokeys}`);
         
         if (!res.ok) {
